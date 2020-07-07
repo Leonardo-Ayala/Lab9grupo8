39da4c5788a85722c7cb8de8945bfa0388e19fcf
Leonardo <a20125980@pucp.pe>
2020-07-06 19:22:41 -0500

    lab9 g8

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-//const fs = require('fs');
 
-/*
-//sincrono
-var textfile = fs.readFileSync('Hola.txt');
-console.log(textfile.toString());
-console.log("Fin del programa");*/
-
-/*
-//asincrono
-fs.readFile("Hola.txt",function (error,info){
-    console.log(info.toString());
-});
-console.log("Fin del programa");*/
-
-//esta parte se copia
 'use strict'
 const express = require("express");
 const path = require("path");
@@ -21,7 +6,6 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
-// la parte superior se copia
 
 app.listen(8080,function (){
     console.log("Servidor levantado correctamente");
@@ -44,92 +28,3 @@ connection.connect(function (error) {
 //hasta aqui se prueba la conneccion a la DB
 
 
-app.get("/listarProductos",function (request,response) {
-    var query = "select * from products";
-    connection.query(query,function (error,result) {
-        if(error){
-            console.log(error);
-        }else{
-            var prueba = [];
-            result.forEach((res) => {
-                //console.log(`${res.ProductName} lives in`);
-                prueba.push({nombre:res.ProductName,
-                                id:res.ProductID});
-            });
-            console.log(result.length);
-            response.json(prueba);
-        }
-    });
-});
-
-app.get("/listarProductosFor",function (request,response) {
-    var query = "select * from products";
-    connection.query(query,function (error,result) {
-        if(error){
-            console.log(error);
-        }else{
-            var prueba = [];
-            var i;
-            for(i=0;i<result.length;i++){
-                prueba.push({nombre:result[i].ProductName,
-                            id:result[i].ProductID});
-            }
-            console.log(result.length);
-            response.json(prueba);
-        }
-    });
-});
-
-
-app.get("/obtenerProducto/:id",function (request,response) {
-    var id = request.params.id;
-    var query = "select * from products where productid = ?";
-    connection.query(query,[id],function (error,result) {
-        if(error){
-            console.log(error);
-        }else{
-            var prueba = [];
-            prueba.push({idproducto:result[0].ProductName});
-            console.log(prueba);
-            response.json(prueba);
-        }
-    });
-});
-
-app.get("/",function (request,response){
-    response.send("Hola Leo");
-});
-
-
-app.get("/pucp/:nombre",function (request,response){
-    var nombre = request.params.nombre;
-    console.log("Nueva solicitud a /pucp/"+nombre);
-    response.send("Hola "+ nombre);
-});
-
-//se recibe parametro con ?name=algo
-app.get("/telecom",function (request,response){
-    var nombre = request.query.nombre;
-    console.log("Nueva solicitud a /telecom?nombre="+nombre);
-    response.send("Hola "+ nombre);
-});
-
-/*parametros por post
-*nombre
-*apellido
-*POSTMAN POST: BODY/x-www-form... */
-app.post("/post",function (request,response){
-    var nombre = request.body.nombre;
-    var apellido = request.body.apellido;
-    //response.send(`Nombre: ${nombre} | Apellido: ${apellido}`);
-    var jsonRespuesta = {data:{nombre:nombre,apellido: apellido}};
-    //var jsonRespuestaOpt = {"nombre":nombre, "apellido":apellido};
-    response.json(jsonRespuesta);
-});
-
-//se RECIBE un json JSON json JSON
-//Postman con POST: BODY/RAW/JSON
-app.post("/hola",express.json(), function (request,response){
-    response.json(request.body);
-    console.log(request.body);
-});
